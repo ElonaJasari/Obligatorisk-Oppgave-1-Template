628077d2c44f554a38ef740f9e5d9afa59b89817
ElonaJasari <s375275@oslomet.no>
2026-09-16 11:52:20 +0200

    Update Oblig1.java

--- src/main/java/no/oslomet/cs/algdat/Oblig1.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig1.java
@@ -6,18 +6,71 @@ public class Oblig1 {
 
     // Oppgave 0
     public static int gruppeMedlemmer() {
-        throw new UnsupportedOperationException();
+        return 1;
         // Skal returne hvor mange som er i gruppa deres.
     }
 
     // Oppgave 1
-    public static int maks(int[] a) {throw new UnsupportedOperationException();}
+    public static int maks(int[] a) {
+        if (a.length < 1){
+            throw new java.util.NoSuchElementException("Tabellen er tom");
+        }
 
-    public static int ombyttinger(int[] a) {throw new UnsupportedOperationException();}
+        for (int i = 0; i < a.length - 1; i++){
+            if (a[i] > a[i + 1]){ // hvis indeks i er større enn i + 1
+                int temp = a[i]; // lagre verdien i indeks i i en temp variabel
+                a[i] = a[i + 1]; // sett den minste verdien før den største
+                a[i + 1] = temp; // sett den største verdien (som ligger i temp) til der den tidligere minste verdien var
+            }
+        }
+        return a[a.length - 1]; // returnerer d største elementet som nå er bakerst
+
+        // a) det blir n - 1 sammenligninger
+        // b) det blir færrest ombyttinger hvis tabellen er i stigende rekkefølge
+        // c) det blir flest ombyttinger hvis den største verdien ligger først
+        // d) ?????
+    }
+
+    public static int ombyttinger(int[] a) {
+        if (a.length < 1){
+            throw new java.util.NoSuchElementException("Tabellen er tom");
+        }
+
+        int ombyttinger = 0;
+        for (int i = 0; i < a.length - 1; i++){
+            if (a[i] > a[i + 1]){ 
+                int temp = a[i]; 
+                a[i] = a[i + 1]; 
+                a[i + 1] = temp; 
+                ombyttinger++;
+            }
+        }
+        return ombyttinger;
+    }
+
 
     // Oppgave 2
     public static int antallUlikeSortert(int[] a) {throw new UnsupportedOperationException();}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
     // Oppgave 3
     public static int antallUlikeUsortert(int[] a) {throw new UnsupportedOperationException();}
 
